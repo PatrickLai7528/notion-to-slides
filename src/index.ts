81351deed4e46356db29645a04bd0e1ba111cd6a
patricklai <patricklai@tencent.com>
2024-11-08 14:35:23 +0800

    change to pageid

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,45 @@
 #!/usr/bin/env node
 
-import yargs from 'yargs'
-import express from 'express'; 
+// import yargs from "yargs";
+import express from "express";
 
-import notion2md from './notion2md';
-import md2slides from './md2slides';
-import { getPageId } from './utils';
+import notion2md from "./notion2md";
+import md2slides from "./md2slides";
+import { getPageId } from "./utils";
 
-const args = yargs
-  .scriptName('notion2slides')
-  .usage('Usage: $0 <command> [options]')
-  .option('url', {
-    alias: 'u',
-    describe: 'The url of the Notion page to convert',
-    type: 'string',
-    demandOption: true
-  })
-  .option('theme', {
-    alias: 't',
-    describe: 'The theme to use for the slides',
-    type: 'string',
-    default: 'default',
-    demandOption: false
-  })
-  .help()
-  .parseSync()
+// const args = yargs
+// 	.scriptName("notion2slides")
+// 	.usage("Usage: $0 <command> [options]")
+// 	.option("url", {
+// 		alias: "u",
+// 		describe: "The url of the Notion page to convert",
+// 		type: "string",
+// 		demandOption: true,
+// 	})
+// 	.option("theme", {
+// 		alias: "t",
+// 		describe: "The theme to use for the slides",
+// 		type: "string",
+// 		default: "default",
+// 		demandOption: false,
+// 	})
+// 	.help()
+// 	.parseSync();
 
 // check the env variable
-const NOTION_TOKEN: string | undefined = process.env.NOTION_TOKEN;
+const NOTION_TOKEN: string | undefined =
+	"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";
 if (!NOTION_TOKEN) {
-  console.error('Please set the NOTION_TOKEN env variable')
-  process.exit(1)
+	console.error("Please set the NOTION_TOKEN env variable");
+	process.exit(1);
 }
 
 // get the url and extract page id from it
-const url = args.url as string;
-const pageId = getPageId(url);
+// const url = args.url as string;
+// const pageId = getPageId(url);
 
 // get the theme from the --theme flag
-const theme = args.theme as string;
+// const theme = args.theme as string;
 
 // prepare to open the file in the browser
 // const opener = require('opener');
@@ -47,17 +48,27 @@ const theme = args.theme as string;
 const app = express();
 const port = 8080;
 
-app.get('/', async (req: express.Request, res: express.Response) => {
-  const mdString = await notion2md(pageId, NOTION_TOKEN);
-  const htmlString = md2slides(mdString, theme);
-  res.send(`
+app.get("/", async (req: express.Request, res: express.Response) => {
+	const pageId = req.query.pageId;
+	const theme = req.query.theme;
+
+	if (!pageId || !theme) {
+		res.send("need pageId and theme query");
+		return;
+	}
+
+	// console.log("here", url);
+	// const pageId = getPageId(url as string);
+	const mdString = await notion2md(pageId as string, NOTION_TOKEN);
+	const htmlString = md2slides(mdString, theme as string);
+	res.send(`
     <html>
     <head>
       <title>Notion to Slides</title>
       <script>
         setInterval(async () => {
           try {
-            const response = await fetch('/');
+            const response = await fetch('/?pageId=${pageId}&theme=${theme}');
             if (!response.ok) {
               throw new Error('Network response was not ok');
             }
@@ -80,5 +91,5 @@ app.get('/', async (req: express.Request, res: express.Response) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+	console.log(`Example app listening at http://localhost:${port}`);
 });
